4330a1511a340f79a6ba2064bcc29e003af548f2
M-b-a-s <mbas750@gmail.com>
2023-09-18 08:16:31 +0100

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: character to check
+ * Return: 0 (success)
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	for (; *s != '\0'; s++)
+		len++;
+	return (a);
+}
