6491098b826f3a648901722b46ac74425d715cd7
M-b-a-s <mbas750@gmail.com>
2023-09-26 03:39:44 +0100

    diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagrams - prints the sum of the two diagonals of a square matrix
+ * @a: pointer to start of of the matrix
+ * @size: width of matrix column
+ * Return: void
+ */
+void print diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -9,5 +12,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack,char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
-
+void print_diagsums(int*a, int size);
 #endif
