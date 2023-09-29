2b720acb169dd0a778db3bf33e3ad7f00a8c76bc
M-b-a-s <mbas750@gmail.com>
2023-09-29 04:06:46 +0100

    fix array values

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,9 +9,9 @@
  */
 int verify(char *s1, char *s2, int i, int j)
 {
-	if (s1[1] == '\0' && s2[j] == '\0')
+	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
-	if (s1[s1] == s2[j])
+	if (s1[i] == s2[j])
 		return (verify(s1, s2, i + 1, j + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (verify(s1, s2, i, j + 1));
