f1e534adccd177dd89d7e011f7b378132121edf4
M-b-a-s <mbas750@gmail.com>
2023-09-29 03:11:58 +0100

    initial commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * verify - checks if number is a prime number
+ * @x: int
+ * @y: int
+ * Return: 0
+ */
+int verify(int x, int y)
+{
+	if (y < 2 || y % x == 0)
+		return (0);
+	else if (a > b / 2)
+		return (1);
+	else
+		return (verify(a + 1, b));
+}
+/**
+ * is_prime_number - returns 1 if the input integer is a number
+ * @n: given integer
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (verify(2, n));
+}
