b977a3de3507b76d455fbb001440f2c205c091a8
M-b-a-s <mbas750@gmail.com>
2023-09-29 01:23:04 +0100

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string followed by a new line
+ * @s: string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putschar(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
