98ca2bcb0e5be3e6f6ea289030e35841dd53d130
M-b-a-s <mbas750@gmail.com>
2023-09-29 03:16:15 +0100

    use correct variables

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,10 +9,10 @@ int verify(int x, int y)
 {
 	if (y < 2 || y % x == 0)
 		return (0);
-	else if (a > b / 2)
+	else if (x > y / 2)
 		return (1);
 	else
-		return (verify(a + 1, b));
+		return (verify(x + 1, y));
 }
 /**
  * is_prime_number - returns 1 if the input integer is a number
