f9874c8c0c766f8ab448ceeffe84663b8b1571bd
M-b-a-s <mbas750@gmail.com>
2023-10-03 04:15:40 +0100

    correct linter error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints name, followed by a new line
+ * main - prints its name, followed by a new line
  * @argc: argument count
  * @argv: argument array
- * Return 0.
+ * Return: 0.
  */
 int main(int argc, char **argv)
 {
