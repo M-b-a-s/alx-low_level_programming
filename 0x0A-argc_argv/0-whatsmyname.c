cf914d1c61747b6cd0c1315256ce1e4499b01a6f
M-b-a-s <mbas750@gmail.com>
2023-10-03 04:12:12 +0100

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints name, followed by a new line
+ * @argc: argument count
+ * @argv: argument array
+ * Return 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
